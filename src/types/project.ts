1483ec0e0edcf65739174ccfaea2eca1bd404ca1
Johnson1z <83126532+JohnsonHuang4396@users.noreply.github.com>
2025-02-17 17:42:29 +0800

    fix: 修复类型错误

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,12 +1,12 @@
 import type { IInnerUploadOptions, IInnerUploadResult } from 'miniprogram-ci/dist/@types/ci/upload'
 
-export interface IPreviewOptions extends Omit<IInnerUploadOptions, 'project'> {
+export interface IPreviewOptions extends Omit<IInnerUploadOptions, 'project' | 'version'> {
   test?: true
 }
 
 export interface IUploadOptions extends Omit<
   IInnerUploadOptions,
-  'project' | 'test' | 'bigPackageSizeSupport' | 'qrcodeFormat' | 'qrcodeOutputDest' | 'pagePath' | 'searchQuery' | 'scene'
+  'project' | 'test' | 'bigPackageSizeSupport' | 'qrcodeFormat' | 'qrcodeOutputDest' | 'pagePath' | 'searchQuery' | 'scene' | 'version'
 > {
 }
 
